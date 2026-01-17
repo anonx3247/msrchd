ae21dc170882ce6de8b330542e6337504057d860
Anas Lecaillon <anas@lecaillon.com>
2026-01-17 01:15:31 +0100

    Added GPT 5.2

--- src/models/openai.ts
+++ src/models/openai.ts
@@ -30,6 +30,7 @@ function normalizeTokenPrices(
 
 // https://platform.openai.com/docs/pricing
 const TOKEN_PRICING: Record<OpenAIModel, OpenAITokenPrices> = {
+  "gpt-5.2": normalizeTokenPrices(1.75, 14),
   "gpt-5": normalizeTokenPrices(1.25, 10),
   "gpt-5-codex": normalizeTokenPrices(1.25, 10),
   "gpt-5.1": normalizeTokenPrices(1.25, 10),
@@ -48,6 +49,7 @@ export function convertThinking(thinking: boolean | undefined) {
 }
 
 export type OpenAIModel =
+  | "gpt-5.2"
   | "gpt-5.1"
   | "gpt-5.1-codex"
   | "gpt-5"
@@ -57,6 +59,7 @@ export type OpenAIModel =
   | "gpt-4.1";
 export function isOpenAIModel(model: string): model is OpenAIModel {
   return [
+    "gpt-5.2",
     "gpt-5.1",
     "gpt-5.1-codex",
     "gpt-5",
@@ -340,6 +343,7 @@ export class OpenAILLM extends LLM {
       case "gpt-5-nano":
       case "gpt-5-codex":
       case "gpt-5.1-codex":
+      case "gpt-5.2":
         return 400000 - 128000;
       case "gpt-4.1":
         return 1047576 - 32768;
